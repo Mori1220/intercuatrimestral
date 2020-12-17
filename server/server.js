06bde4b7b2c03f0830fc80d26289f831f68b1c28
Karla.Rodriguez <moriAeseces@gmail.com>
2020-12-16 18:25:50 -0600

    Conexion lista a BD

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require('./config/config')
 const express = require('express')
+const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const app = express()
 
@@ -63,6 +64,18 @@ app.delete('/usuario', function(req, res){
 
     })
 })
+
+mongoose.connect('mongodb://localhost:27017/administracion', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+    
+},(err, res) => {
+    if (err) throw err;
+    console.log ('Base de datos ONLINE')
+    
+});
 
 app.listen(process.env.PORT, () => {
     console.log('El servidor esta en linea en el puerto ', process.env.PORT)
